d10032a77a1f31d2921cda95b5b6c216d553576a
Alex <119821293+Devallex@users.noreply.github.com>
2024-03-24 14:09:48 -0400

    api.js overwrites form behavior
    - Allows for more methods
    - Auto-assumes /api/ url
    - Handles certain responses

--- project/website/static/resources/js/api.js
+++ project/website/static/resources/js/api.js
@@ -7,6 +7,8 @@ function call_error(code) {
 function call_api(url, method = "GET", callback = () => { }, body = {}) {
 	url = API_URL + url
 
+	method = method.toUpperCase()
+
 	if (method == "GET") {
 		body = null
 	} else {
@@ -29,5 +31,58 @@ function call_api(url, method = "GET", callback = () => { }, body = {}) {
 		})
 		.then(callback)
 }
+
+Array.prototype.slice.call(document.getElementsByTagName("form")).forEach((form) => {
+	form.addEventListener("submit", (event) => {
+		event.preventDefault()
+
+		var formData = new FormData(form);
+
+		const action = form.getAttribute("action")
+		const method = form.getAttribute("method").toUpperCase()
+
+		if (method == "GET") {
+			body = null
+		} else {
+			body = formData
+		}
+
+		fetch("/api" + action, {
+			method: method,
+			body: JSON.stringify(Object.fromEntries(formData))
+		})
+			.then(response => {
+				if (response.redirected) {
+					window.location.href = response.url
+					return
+				}
+
+				const contentType = response.headers.get('Content-Type');
+
+				if (contentType) {
+					if (contentType.includes("application/json")) {
+						response.text().then((text) => {
+							alert(text)
+						})
+					} else if (contentType.includes("text/html")) {
+						response.text().then((html) => {
+							alert(html)
+						})
+					} else {
+						alert("ERROR: Unknown response type!")
+					}
+				} else {
+					alert("ERROR: Unspecified response type!")
+				}
+			})
+			.catch(error => {
+				console.log("ERROR", error)
+
+				alert("ERROR: " + error)
+				console.error("Form error: ", error)
+			});
+
+	})
+})
 
 loaded()
