51ba7090a02deb004d92fc38ac82f9e3e012ed46
Alex <119821293+Devallex@users.noreply.github.com>
2024-03-24 14:10:37 -0400

    Created users frontend

---
+++ project/website/static/resources/js/users.js
@@ -0,0 +1,49 @@
+function createSession(data) {
+	const date = new Date(data.expires)
+	document.cookie = `session=${data.id + "." + data.user_id + "." + data.auth}; expires=${date.toUTCString()}; path=/`
+}
+
+function getSession() {
+	session = getCookie("session")
+
+	if (!session) {
+		return
+	}
+
+	components = session.split(".")
+
+	return {
+		"id": components[0],
+		"user_id": components[1],
+		"token": components[2]
+	}
+}
+
+function isLoggedIn() {
+	return getSession() != null
+}
+
+function getUserInfo(callback) {
+	if (!isLoggedIn()) {
+		return
+	}
+
+	session = getSession()
+
+	call_api("/users/" + session.user_id, method="GET", callback = callback)
+}
+
+// Validate
+if (getSession()) {
+	call_api("/sessions/validate/", method = "GET", callback = (valid) => {
+		if (valid) {
+			return
+		}
+
+		deleteCookie("session")
+
+		location.reload()
+	})
+}
+
+loaded()
