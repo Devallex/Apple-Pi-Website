39b85121b19b5dca231cf9d9556ae66383de0d09
Alex <119821293+Devallex@users.noreply.github.com>
2024-04-07 13:18:50 -0400

    Modified footer

--- project/website/static/resources/js/footer.js
+++ project/website/static/resources/js/footer.js
@@ -2,27 +2,24 @@ Array.prototype.slice.call(document.getElementsByTagName("footer")).forEach((foo
 	const textContent = footer.textContent
 	footer.innerHTML = `
 		<div class="background"></div>
-		<div class="content">
+		<div class="content center">
 			${textContent}
-			<h1>Find Us</h1>
 			<address>
-				<h2>Contact</h2>
 				<p>
+					<label>Contact:</label>
 					<a href="mailto:${CONTACT_EMAIL}">${CONTACT_EMAIL}</a>
 				</p>
-				<h2>Social Media</h2>
 				<p>
-					<a href="https://www.facebook.com/people/Apple-Pi-Robotics-FRC-Team-2067/100054299360253/">Facebook</a
-					<br>
-					<a href="https://www.twitter.com/FRC2067">Twitter</a
-					<br>
+					<label>Social Media:</label>
+					<a href="https://www.facebook.com/people/Apple-Pi-Robotics-FRC-Team-2067/100054299360253/">Facebook</a> |
+					<a href="https://www.twitter.com/FRC2067">Twitter</a>
 				</p>
-				<h2>Address</h2>
-				29 Soundview Rd.
-				<br>
-				Building 2, Unit #3
-				<br>
-				Guilford, CT 06437
+				<p>
+					<label>Address:</label>
+					29 Soundview Rd.
+					Building 2, Unit #3
+					Guilford, CT 06437
+				</p>
 			</address>
 		</div>
 	`
