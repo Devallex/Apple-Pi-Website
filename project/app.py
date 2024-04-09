3bae9324c6ed2c6e58c93ced22c3f096dded8f3d
Alex <119821293+Devallex@users.noreply.github.com>
2024-04-08 21:21:34 -0400

    Add support for duckdns

--- project/app.py
+++ project/app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 from os import getenv
+import requests
 
 load_dotenv()
 
@@ -32,6 +33,26 @@ def on_create_all(callback):
     on_create_all_callbacks.append(callback)
 
 
+last_ip = None
+domain = getenv("DUCKDNS_DOMAIN")
+token = getenv("DUCKDNS_TOKEN")
+
+
+def update_ip():
+    global last_ip
+
+    current_ip = requests.get("https://api.ipify.org").content.decode("utf8")
+    if current_ip != last_ip:
+        print(
+            "IP change detected! Old IP is '%s', new IP is '%s'."
+            % (last_ip, current_ip)
+        )
+        last_ip = current_ip
+        requests.get(
+            "https://www.duckdns.org/update/%s/%s/%s" % (domain, token, current_ip)
+        )
+
+
 def run():
     with app.app_context():
         db.create_all()
@@ -46,9 +67,24 @@ def run():
         "prod",
     ), "You must specify the MODE in the .env file!"
     if mode == "prod":
+        domain = getenv("DUCKDNS_DOMAIN")
+        token = getenv("DUCKDNS_TOKEN")
+
+        if domain and token:
+            print("DuckDNS is enabled for domain '%s'." % domain)
+            scheduler.add_job(update_ip, "interval", seconds=10)
+            update_ip()
+
         from waitress import serve
-        print("Running production server on %s:%s, press CTRL/CMD + C to exit." % (getenv("PROD_HOST"), getenv("PROD_PORT")))
-        print("Use password '%s' to log into the '%s' account." % (getenv("ADMIN_PASSWORD"), getenv("ADMIN_USERNAME")))
+
+        print(
+            "Use password '%s' to log into the '%s' account."
+            % (getenv("ADMIN_PASSWORD"), getenv("ADMIN_USERNAME"))
+        )
+        print(
+            "Running production server on %s:%s, press CTRL/CMD + C to exit."
+            % (getenv("PROD_HOST"), getenv("PROD_PORT"))
+        )
         print()
         serve(app, host=getenv("PROD_HOST"), port=getenv("PROD_PORT"))
     else:
